d8e7e67e5245fa331ebf810a1515c95aef5f626e
ruchita-oza <70565213+ruchita-oza@users.noreply.github.com>
2021-09-02 18:19:10 +0530

    navbar half

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,23 @@
-import React from 'react'
-
-function navbar() {
-    return (
-        <div>
-            
-        </div>
-    )
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+function Navbar() {
+  const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" classname="navbar-logo">
+          PetFinder <i class="fas fa-paw"></i>
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-window-close" : "fas fa-bars"}></i>
+              </div>
+              <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                  <li clasName="nav-item" onClick={closeMobileMenu}>Home</li>
+              </ul>
+      </div>
+    </nav>
+  );
 }
-
-export default navbar
+export default Navbar;
